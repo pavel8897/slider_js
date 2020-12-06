d7e85178b70a6a6270bf0ec3d8f6de147e1038b5
pavel <pavel8897@bk.ru>
2020-12-06 20:17:52 +0300

    slider

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" href="style.css">	
+</head>
+<body>
+	<div class="wrapper">
+		<div class="slider-container">
+			<div class="slider-track">
+				<div class="slider-item"></div>
+				<div class="slider-item"></div>
+				<div class="slider-item"></div>
+				<div class="slider-item"></div>
+				<div class="slider-item"></div>
+				<div class="slider-item"></div>
+				<div class="slider-item"></div>
+				<div class="slider-item"></div>
+				<div class="slider-item"></div>
+			</div>
+		</div>
+		<div class="slider-buttons">
+			<button class="btn-prev">Prev</button>
+			<button class="btn-next">Next</button>
+		</div>
+	</div>
+	
+	<script src="script.js"></script>
+</body>
+</html>
